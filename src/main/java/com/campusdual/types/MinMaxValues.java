d8a47d8a4b29c12e1c418de71afd2d99ccb87d3f
Ricardo Lopez Peña <ricardoslopezs2001@gmail.com>
2024-10-09 16:05:32 +0200

    clase de separacion de grafias de valores

---
+++ src/main/java/com/campusdual/types/MinMaxValues.java
@@ -0,0 +1,34 @@
+package com.campusdual.types;
+public class MinMaxValues {
+    public static void main(String[] args) {
+
+        System.out.println("Minimum byte value: " +Byte.MIN_VALUE);
+        System.out.println("Maximum byte value: " + Byte.MAX_VALUE);
+
+
+        System.out.println("Minimum short value: " +Short.MIN_VALUE);
+        System.out.println("Maximum short value: " + Short.MAX_VALUE);
+
+        System.out.println("Minimum Integer value: " + Integer.MIN_VALUE);
+        System.out.println("Maximum Integer value: " + Integer.MAX_VALUE);
+
+        System.out.println("Minimum Long value: " + Long.MIN_VALUE);
+        System.out.println("Maximum Long value: " + Long.MAX_VALUE);
+
+        System.out.println("Minimum Float value: " + Float.MIN_VALUE);
+        System.out.println("Maximum Float value: " + Float.MAX_VALUE);
+
+        System.out.println("Minimum double value: " + Double.MIN_VALUE);
+        System.out.println("Maximum double value: " + Double.MAX_VALUE);
+
+
+
+
+
+
+
+
+
+
+    }
+}
