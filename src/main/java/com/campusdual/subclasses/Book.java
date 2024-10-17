511a75d51add958a39587ef8c81141bbcc0847d3
Ricardo Lopez Peña <ricardoslopezs2001@gmail.com>
2024-10-17 11:06:01 +0200

    Clase Pablo, finalizada. 17/10/2024

--- src/main/java/com/campusdual/subclasses/Book.java
+++ src/main/java/com/campusdual/subclasses/Book.java
@@ -1,11 +1,13 @@
 package com.campusdual.subclasses;
 
-public class Book extends Item{
+public class Book extends Item/*aqui ponemos "extends Item" porque vamos a extender mas variables,
+        a mayores de las variables definidas en items,*/{
     private String author;
     private String isbn;
     //COMO MINIMO el constructor de esta clase
     // debe llamar al constructor de la clase base
     public Book(String title, String author, String publicationDate, String isbn){
+        //aqui ponemos super porque son variables ya definidas en la clase items
         super(title, publicationDate);
         this.author = author;
         this.isbn = isbn;
